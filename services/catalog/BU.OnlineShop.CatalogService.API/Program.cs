249c8178544422532d3a019ef2b4c190eca04a6c
Baturalp Uyar <baturalp.uyar@siemens.com>
2025-06-12 11:56:48 +0300

    fix: test

--- services/catalog/BU.OnlineShop.CatalogService.API/Program.cs
+++ services/catalog/BU.OnlineShop.CatalogService.API/Program.cs
@@ -36,7 +36,7 @@ Log.Logger = new LoggerConfiguration()
             options.ResourceAttributes = new Dictionary<string, object>
             {
                 ["service.name"] = "CatalogService"
-            }; 
+            };
         })
         .WriteTo.File(path: "Logs/logs.txt", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 31, fileSizeLimitBytes: 536870912)
         .WriteTo.Console()
